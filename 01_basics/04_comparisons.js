57b5d91f68a627813322af66b23331521ffd7c49
friday-18 <124078515+friday-18@users.noreply.github.com>
2023-01-31 19:57:13 +0000

    compare

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// the above confusions have to be avoided 
+// acha code likhna hai confusions nahi 
+// === 
+
+console.log("2" === 2);
