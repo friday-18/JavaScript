1e1da4ff08a0cf6897b53f73e5bf431bbed30411
friday-18 <124078515+friday-18@users.noreply.github.com>
2023-01-31 18:53:51 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,26 @@
 
 // alert(9+9) // we are using node js , not browser !
 // abc
+
+let name = " FRIday "
+let age = 18
+let isLoggedin = false
+
+// number -> 2 raise to 53
+// bigINT
+// string -> " useDouble quotes"
+// boolean --> dichotomy --> true/false
+// null -> standalone value {representation of empty}
+// undefined --> STILL THE VALUE IS NOT ASSIGNED 
+// symbol -> unique
+
+
+
+// object
+
+
+console.log(typeof "Friday");  // string
+console.log(typeof age); // number
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined
+
